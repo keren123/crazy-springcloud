983ce753f675f3f28a94ca7738b464f00862b262
coder0001 <coder0001>
2021-10-26 21:25:23 +0800

    提交代码

---
+++ cloud-center-alibaba/ha-middleware-demo/src/main/java/com/crazymaker/cloud/ha/middleware/starter/HaMiddleWareApplication.java
@@ -0,0 +1,78 @@
+package com.crazymaker.cloud.ha.middleware.starter;
+
+import com.crazymaker.springcloud.standard.config.FeignConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableAutoConfiguration(exclude = {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+})
+
+@SpringBootApplication(scanBasePackages =
+        {
+                "com.crazymaker.cloud.ha.middleware",
+                "com.crazymaker.springcloud.demo",
+                "com.crazymaker.springcloud.base",
+                "com.crazymaker.springcloud.user",
+                "com.crazymaker.springcloud.seckill.remote.fallback",
+                "com.crazymaker.springcloud.standard"
+        }, exclude = {SecurityAutoConfiguration.class})
+@EnableScheduling
+@EnableSwagger2
+@EnableJpaRepositories(basePackages = {
+//        "com.crazymaker.springcloud.user.*.dao",
+        "com.crazymaker.springcloud.base.dao"
+})
+
+
+@EntityScan(basePackages = {
+        "com.crazymaker.springcloud.user.*.dao.po",
+        "com.crazymaker.springcloud.base.dao.po",
+        "com.crazymaker.springcloud.standard.*.dao.po"})
+/**
+ * 启用 Hystrix
+ */
+@EnableHystrix
+@EnableFeignClients(
+        basePackages = "com.crazymaker.springcloud.user.info.remote.client",
+        defaultConfiguration = FeignConfiguration.class)
+@EnableTransactionManagement
+@Slf4j
+public class HaMiddleWareApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(HaMiddleWareApplication.class, args);
+
+        Environment env = applicationContext.getEnvironment();
+        String port = env.getProperty("server.port");
+        String name = env.getProperty("spring.application.name");
+
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(path)) {
+            path = "";
+        }
+        String ip = env.getProperty("spring.cloud.client.ip-address");
+
+        System.out.println("\n----------------------------------------------------------\n\t" +
+                name.toUpperCase() + " is running! Access URLs:\n\t" +
+                "Local: \t\thttp://" + ip + ":" + port + path + "/\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
+                "actuator: \thttp://" + ip + ":" + port + path + "/actuator/info\n\t" +
+                "----------------------------------------------------------");
+    }
+}
